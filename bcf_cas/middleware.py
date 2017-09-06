077d138d0c8ff572500062c5398dfb69c25089f8
Michelle Yung <myung@email.arizona.edu>
2017-09-06 16:00:29 -0700

    is_authenticated() --> is_authenticated

--- bcf_cas/middleware.py
+++ bcf_cas/middleware.py
@@ -48,7 +48,7 @@ class CASMiddleware(object):
         elif not view_func.__module__.startswith('django.contrib.admin.'):
             return None
 
-        if request.user.is_authenticated():
+        if request.user.is_authenticated:
             if request.user.is_staff:
                 return None
             else:
